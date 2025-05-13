0da45d5c0323464f86072364e4fb4249113f60bd
Хобенков Егор <khobenkov@yandex.ru>
2025-05-13 16:10:45 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 400,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 850,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 670,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1867,
+        price: 1200,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Юваль Ной Харари',
+        name: 'Sapiens. Краткая история человечества',
+        publicationYear: 2020,
+        price: 750,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Стивен Хокинг',
+        name: 'Краткая история времени',
+        publicationYear: 1988,
+        price: 600,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Нассим Талеб',
+        name: 'Черный лебедь',
+        publicationYear: 2007,
+        price: 730,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Рэй Далио',
+        name: 'Принципы',
+        publicationYear: 2017,
+        price: 950,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Даниэль Канеман',
+        name: 'Думай медленно... решай быстро',
+        publicationYear: 2011,
+        price: 880,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Морган Хаузел',
+        name: 'Психология денег',
+        publicationYear: 2021,
+        price: 620,
+        bestseller: true,
+        available: true
+    }
+];
